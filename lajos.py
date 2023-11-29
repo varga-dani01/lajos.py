82302a30ec5d15f862c530478948a4c34ea62ead
varga-dani01 <90318466+varga-dani01@users.noreply.github.com>
2023-11-29 12:55:43 +0100

    Update lajos.py

--- lajos.py
+++ lajos.py
@@ -4,3 +4,19 @@ import streamlit as st
  
 # Title
 st.title("Hello GeeksForGeeks !!!")
+
+from tkinter import *
+
+def evaluate_expression(expression):
+    try:
+        result = str(eval(expression))
+        entry_var.set(result)
+    except Exception as e:
+        entry_var.set("Hiba")
+
+def button_click(symbol):
+    current_text = entry_var.get()
+    entry_var.set(current_text + str(symbol))
+
+def clear_entry():
+    entry_var.set("")
