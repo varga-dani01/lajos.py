085f2d5af151a60dcc98f3750c3c296c4c1c6157
varga-dani01 <90318466+varga-dani01@users.noreply.github.com>
2023-11-29 13:08:14 +0100

    Update lajos.py

--- lajos.py
+++ lajos.py
@@ -6,26 +6,45 @@ import streamlit as st
 st.title("Hello GeeksForGeeks !!!")
 
 
-st.title("Streamlit Számológép")
+import streamlit as st
 
-num1 = st.number_input("Adj meg egy számot", step=1.0)
-operator = st.selectbox("Válassz műveletet", ('+', '-', '*', '/'))
-num2 = st.number_input("Adj meg még egy számot", step=1.0)
+def add(x, y):
+    return x + y
 
-if st.button("Számol"):
-    if operator == '+':
-        result = num1 + num2
-    elif operator == '-':
-        result = num1 - num2
-    elif operator == '*':
-        result = num1 * num2
-    elif operator == '/':
-        if num2 != 0:
-            result = num1 / num2
-        else:
-            result = "Hiba: osztás 0-val"
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Hiba: osztás 0-val"
+
+def calculate(selected_operation, num1, num2):
+    if selected_operation == "Összeadás":
+        result = add(num1, num2)
+    elif selected_operation == "Kivonás":
+        result = subtract(num1, num2)
+    elif selected_operation == "Szorzás":
+        result = multiply(num1, num2)
+    elif selected_operation == "Osztás":
+        result = divide(num1, num2)
     else:
         result = "Érvénytelen művelet!"
+    return result
 
-    st.success(f"Eredmény: {result}")
+st.title("Extreme Streamlit Számológép")
+
+selected_operation = st.selectbox("Válassz műveletet", ("Összeadás", "Kivonás", "Szorzás", "Osztás"))
+
+num1 = st.number_input("Adj meg egy számot", step=1.0)
+num2 = st.number_input("Adj meg még egy számot", step=1.0)
+
+result = calculate(selected_operation, num1, num2)
+st.success(f"Eredmény: {result}")
+
+st.info("Ez egy interaktív Streamlit számológép példa.")
 
