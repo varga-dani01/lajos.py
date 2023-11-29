59cf1a8f4cfc02f1b2b3d5385b01cb94b0526a7d
varga-dani01 <90318466+varga-dani01@users.noreply.github.com>
2023-11-29 12:57:41 +0100

    Update lajos.py

--- lajos.py
+++ lajos.py
@@ -21,16 +21,16 @@ def button_click(symbol):
 def clear_entry():
     entry_var.set("")
 
-# A fő alkalmazás ablak létrehozása
+
 calculator = Tk()
 calculator.title("Számológép")
 
-# Beviteli mező létrehozása
+
 entry_var = StringVar()
 entry = Entry(calculator, textvariable=entry_var, font=("Arial", 18), bd=10, insertwidth=4, width=14, justify="right")
 entry.grid(row=0, column=0, columnspan=4)
 
-# Gombok létrehozása
+
 buttons = [
     ('7', 1, 0), ('8', 1, 1), ('9', 1, 2), ('/', 1, 3),
     ('4', 2, 0), ('5', 2, 1), ('6', 2, 2), ('*', 2, 3),
@@ -42,7 +42,6 @@ for (text, row, col) in buttons:
     button = Button(calculator, text=text, font=("Arial", 14), padx=20, pady=20, command=lambda t=text: button_click(t))
     button.grid(row=row, column=col)
 
-# Törlés gomb
 clear_button = Button(calculator, text="C", font=("Arial", 14), padx=20, pady=20, command=clear_entry)
 clear_button.grid(row=5, column=0, columnspan=3)
 
