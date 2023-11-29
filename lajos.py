2e130deefd7d6dc92dcc9d241520ac08c0329688
varga-dani01 <90318466+varga-dani01@users.noreply.github.com>
2023-11-29 13:00:39 +0100

    Update lajos.py

--- lajos.py
+++ lajos.py
@@ -21,21 +21,30 @@ def divide(x, y):
         return "Hiba: osztás 0-val"
 
 while True:
+    # Felhasználótól input bekérése
     num1 = float(input("Adj meg egy számot: "))
     operator = input("Válassz műveletet (+, -, *, /): ")
     num2 = float(input("Adj meg még egy számot: "))
 
-    if operator == '+':
-        result = add(num1, num2)
-    elif operator == '-':
-        result = subtract(num1, num2)
-    elif operator == '*':
-        result = multiply(num1, num2)
-    elif operator == '/':
-        result = divide(num1, num2)
+    # Művelet végrehajtása és eredmény kiírása
+    if operator in ('+', '-', '*', '/'):
+        result = None
+        if operator == '+':
+            result = add(num1, num2)
+        elif operator == '-':
+            result = subtract(num1, num2)
+        elif operator == '*':
+            result = multiply(num1, num2)
+        elif operator == '/':
+            result = divide(num1, num2)
+        print(f"Eredmény: {result}")
     else:
         print("Érvénytelen művelet!")
-        continue
+
+    # Új számológép használata vagy kilépés
+    another_calculation = input("Szeretnél új számolást végezni? (igen/nem): ")
+    if another_calculation.lower() != 'igen':
+        break
 
     print(f"Eredmény: {result}")
 
